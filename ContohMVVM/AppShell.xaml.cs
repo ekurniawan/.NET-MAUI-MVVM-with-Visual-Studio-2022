e875bea6693d7ab8abb98dc072cb78defc4921f0
erickkurniawan <erick.kurniawan@gmail.com>
2023-07-26 14:07:00 +0700

    contoh maui mvvm

---
+++ ContohMVVM/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ContohMVVM;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ ContohMVVM/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ContohMVVM;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
