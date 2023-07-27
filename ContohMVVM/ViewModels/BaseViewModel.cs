15861b603abddc14d5fcb67f12757f65fd1fc6f3
erickkurniawan <erick.kurniawan@gmail.com>
2023-07-27 10:31:08 +0700

    membuat baseview

---
+++ ContohMVVM/ViewModels/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ContohMVVM.ViewModels
+{
+	public partial class BaseViewModel : ObservableObject
+	{
+		public BaseViewModel()
+		{
+		}
+
+		[ObservableProperty]
+		[NotifyPropertyChangedFor(nameof(isNotBusy))]
+		bool isBusy;
+
+		[ObservableProperty]
+		string title;
+
+		public bool isNotBusy => !IsBusy;
+	}
+}
+
