f78ce9b6b941ea3dfa7d53d5d043ac86dc6c6886
erickkurniawan <erick.kurniawan@gmail.com>
2023-07-28 15:17:41 +0700

    menggunakan Data Editor

---
+++ ContohMVVM/ViewModels/DataEditorViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ContohMVVM.ViewModels
+{
+	public partial class DataEditorViewModel : ObservableObject
+    {
+		[ObservableProperty]
+        string notes = "";
+
+		[ObservableProperty]
+        DateTime? birthDate;
+
+		[ObservableProperty]
+		string phone;
+
+		[ObservableProperty]
+		string login = "";
+
+		[ObservableProperty]
+        bool loginHasError = false;
+
+		[ObservableProperty]
+        string password = "";
+
+		[ObservableProperty]
+        bool passwordHasError = false;
+
+        public bool Validate()
+        {
+            PasswordHasError = !Regex.IsMatch(password, @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{5,}$");
+            LoginHasError = String.IsNullOrEmpty(login);
+            return !PasswordHasError && !LoginHasError;
+        }
+
+        [RelayCommand]
+        async Task SubmitAsync()
+        {
+            if (Validate())
+                await Shell.Current.DisplayAlert("Success", "Your account has been created successfully", "OK");
+        }
+	}
+}
+
---
+++ ContohMVVM/Views/DxEditorPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ContohMVVM.Views;
+
+public partial class DxEditorPage : ContentPage
+{
+	public DxEditorPage()
+	{
+		InitializeComponent();
+	}
+}
