d678e2ecd04384efd3f0f88399a8848cf4b1c854
erickkurniawan <erick.kurniawan@gmail.com>
2023-07-28 16:52:39 +0700

    menambahkan sqlite

---
+++ ContohMVVM/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SQLite;
+
+namespace ContohMVVM.Models
+{
+    [Table("people")]
+	public class Person
+	{
+        [PrimaryKey,AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(250),Unique]
+        public string Name { get; set; }
+    }
+}
+
